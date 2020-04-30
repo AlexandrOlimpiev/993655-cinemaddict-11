21cdac78945b02e74eec8311b621e800a57cf63c
Alexandr Olimpiev <alexandr.olimpiev@gmail.com>
2020-04-30 21:08:58 +0300

    Личный проект: шаблонизируй это

---
+++ src/mocks/data.js
@@ -0,0 +1,20 @@
+const FILM_DATA_KIT = {
+  TITLES: [`Made-for-each-other`, `Popeye-meets-sinbad`, `Sagebrush-trail`, `Santa-claus-conquers-the-martians`, `The-dance-of-life`, `The-great-flamarion`, `The-man-with-the-golden-arm`],
+  POSTERS: [`made-for-each-other.png`, `popeye-meets-sinbad.png`, `sagebrush-trail.jpg`, `santa-claus-conquers-the-martians.jpg`, `the-dance-of-life.jpg`, `the-great-flamarion.jpg`, `the-man-with-the-golden-arm.jpg`],
+  DESCRIPTIONS: [] = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`.split(/ (?=[A-Z])/),
+  GENRES: [`Action`, `Sci-Fi`, `Comedy`, `Adventure`, `Animation`, `Thriller`, `Horror`, `Musical`],
+  NAMES: [`Bennett`, `Foster`, `Adams`, `Jenkins`, `Mitchell`, `Smith`, `King`, `Von`, `Evans`, `Watson`],
+  SURNAMES: [`Axel`, `Zaid`, `Edwin`, `Uchechi`, `Bryson`, `Usher`, `Alexander`, `Sawyer`, `Grey`, `Leonidas`],
+  COUNTRY: [`Argentina`, `USA`, `Brazil`, `China`, `Denmark`, `Greece`, `Italy`, `Mexico`, `Russia`, `Ukraine`]
+};
+
+const COMMENT_DATA_KIT = {
+  EMOJIS: [`angry`, `puke`, `sleeping`, `smile`],
+  TEXTS: [`Interesting setting and a good cast`, `Booooooooooring`, `Very very old. Meh`, `Almost two hours? Seriously?`],
+  AUTHORS: [`Tim Macoveev`, `John Doe`, `Robert Weide`, `Chris Martin`, `Noel Gallagher`]
+};
+
+export {
+  FILM_DATA_KIT,
+  COMMENT_DATA_KIT
+};
